1ab570745cba960e2f2c1b32ff007d199916e1af
Juanliendo22 <105870153+Juanliendo22@users.noreply.github.com>
2024-11-18 15:16:04 -0300

    primer commit

--- app/src/components/navbar/Navbar.jsx
+++ app/src/components/navbar/Navbar.jsx
@@ -1,9 +1,40 @@
+import React, { useState } from "react";
 import "./Navbar.css";
+import Button from "../../components/button/Button";  // Importa el componente Button
+import logo from "../../assets/logo.png";
 
 const Navbar = () => {
-    return (
-        <h1>Navbar</h1>
-    )
-}
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-logo">          <img src={logo} alt="Logo" />
+        </div>
+        <button className="menu-toggle" onClick={toggleMenu}>
+          ☰
+        </button>
+        <ul className={`navbar-links ${isMenuOpen ? "open" : ""}`}>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            <Button />  {/* Reemplaza "Contact" con el botón */}
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
