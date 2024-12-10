ad8577a7e4a5cdff53baa118e26f0974b7093203
Juan <liendojuan070@gmail.com>
2024-12-10 10:13:04 -0300

    mejoras en diseño de la card de cursos

--- app/src/components/CardCourse/CardCourse.jsx
+++ app/src/components/CardCourse/CardCourse.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 import './CardCourse.css';
 import Button from '../button/Button';
+import ButtonVacio from '../buttonVacio/buttonVacio';
+import liveImage from '../../assets/live.png';
+import book from '../../assets/book.png';
+import person from '../../assets/person.png';
+import video from '../../assets/video.png';
 
 const CardCourse = ({
   imageSrc,
@@ -25,24 +30,34 @@ const CardCourse = ({
         <div className="card-course-front">
           <img src={imageSrc} alt={title} className="card-course-image" />
           <h3 className="card-course-title">{title}</h3>
-          <p className="card-course-duration">⏰ {duration}</p>
-          <p className="card-course-modality">💻 {modality}</p>
-          <Button text="Más información" onClick={handleFlip} />
+          <p className="card-course-duration">
+            <img src={video} alt="Duration" className="card-course-icon" /> {duration}
+          </p>
+          <p className="card-course-modality">
+            <img src={liveImage} alt="Modality" className="card-course-icon" /> {modality}
+          </p>
+          <Button  text="Más información" onClick={handleFlip} />
         </div>
 
         {/* Lado Trasero */}
         <div className="card-course-back">
           <p className="card-course-description">{description}</p>
           <ul className="card-course-details">
-            <li>📚 {modules} módulos.</li>
-            <li>🎥 {recordedClasses} clases grabadas al mes.</li>
-            <li>🖥️ {liveClasses} clase en vivo al mes.</li>
-            <li>👨‍🏫 {personalizedAdvice}</li>
+            <li>
+              <img src={book} alt="Modules" className="card-course-icon" /> {modules} módulos.
+            </li>
+            <li>
+              <img src={video} alt="Recorded Classes" className="card-course-icon" /> {recordedClasses} clases grabadas al mes.
+            </li>
+            <li>
+              <img src={liveImage} alt="Live Classes" className="card-course-icon" /> {liveClasses} clase en vivo al mes.
+            </li>
+            <li>
+              <img src={person} alt="Personalized Advice" className="card-course-icon" /> {personalizedAdvice}
+            </li>
           </ul>
-          <Button text="Volver" onClick={handleFlip} />
-          <Button text="Inscribirme" onClick={handleFlip} />
-
-
+          <Button text="Inscribirme " onClick={handleFlip} />
+          <ButtonVacio text="Descargar info" onClick={handleFlip} />
         </div>
       </div>
     </div>
---
+++ app/src/components/buttonVacio/ButtonVacio.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ButtonVacio.css'; // Asegúrate de tener el archivo CSS para los estilos
+
+const ButtonVacio = () => {
+  return (
+    <button className="button-vacio">
+      DESCARGAR INFO
+    </button>
+  );
+};
+
+export default ButtonVacio;
