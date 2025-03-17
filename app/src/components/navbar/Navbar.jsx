5ab181e14f3941b7c0fd53db6d206d7a32c9ed1e
julian gonzalez <69334886+julimax@users.noreply.github.com>
2025-03-17 18:54:37 -0300

    Update Navbar.jsx

--- app/src/components/navbar/Navbar.jsx
+++ app/src/components/navbar/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import "./Navbar.css";
 import Button from "../../components/button/Button"; // Importa el componente Button
 import logo from "../../assets/logo2.png";
@@ -15,23 +14,22 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="navbar-container">
         <div className="navbar-logo">
-          <Link to="/">
-            <img src={logo} alt="Logo" />
-          </Link>
+          {" "}
+          <a href="/">
+  <img src={logo} alt="Logo" />
+</a>
         </div>
         <button className="menu-toggle" onClick={toggleMenu}>
           ☰
         </button>
-        <ul className={navbar-links ${isMenuOpen ? "open" : ""}}>
+        <ul className={`navbar-links ${isMenuOpen ? "open" : ""}`}>
           <li>
             <a href="#conocenos">Conocenos</a>
           </li>
           <li>
             <a href="#cursos">Nuestros cursos</a>
           </li>
-          <li>
-            <Link to="/nosotros">Nosotros</Link> {/* Cambia el enlace a "nosotros" */}
-          </li>
+
           <li>
             <Button text="Plataforma" /> {/* El botón ahora dirá PLATAFORMA */}
           </li>
