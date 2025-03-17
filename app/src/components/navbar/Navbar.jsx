9bce618bd86748b3faef3e5b58fa231426500d3a
julian gonzalez <69334886+julimax@users.noreply.github.com>
2025-03-17 18:46:20 -0300

    Update Navbar.jsx

--- app/src/components/navbar/Navbar.jsx
+++ app/src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-mport React, { useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import Button from "../../components/button/Button"; // Importa el componente Button
